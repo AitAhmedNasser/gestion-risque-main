dc615be4a0f52e26432ac65176e98166d516de59
abdelaziz.araar <h.araar@intervalle-technologies.com>
2022-10-13 12:12:22 +0100

    First commit

---
+++ gestion-risque-main/complete/application/src/main/java/com/intervalle/technologies/multimodule/application/MainApplication.java
@@ -0,0 +1,27 @@
+package com.intervalle.technologies.multimodule.application;
+
+import com.intervalle.technologies.multimodule.service.MyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = "com.intervalle.technologies.multimodule")
+@RestController
+public class MainApplication {
+
+	private final MyService myService;
+
+	public MainApplication(MyService myService) {
+		this.myService = myService;
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return myService.message();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+}
