732ef0962d6068bcb24e578128a260431e0b2299
abdelaziz.araar <h.araar@intervalle-technologies.com>
2022-11-21 10:00:13 +0100

    Fusion des projet gestion-risque-app et Risque-managment-di

--- gestion-risque-main/complete/application/src/main/java/it/gestionRisque/app/Entities/Garantie.java
+++ gestion-risque-main/complete/application/src/main/java/it/gestionRisque/app/Entities/Garantie.java
@@ -1,5 +1,7 @@
 package it.gestionRisque.app.Entities;
 
+import java.util.Map;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -38,6 +40,31 @@ public class Garantie {
 	private String currencyBankingLgColValue;
 
 
-
+	public static Garantie garantieFromJSON(Map<String, String> data) {
+		
+		 Double realEstateColValue = (data.get("REAL_ESTATE_COL_VALUE")!= "" && data.get("REAL_ESTATE_COL_VALUE")!=null)? Double.parseDouble(data.get("REAL_ESTATE_COL_VALUE")):null;
+		 String currencyRealEstateColValue = (data.get("CURRENCY_REAL_ESTATE_COL_VALUE")!= "" && data.get("CURRENCY_REAL_ESTATE_COL_VALUE")!=null)? data.get("CURRENCY_REAL_ESTATE_COL_VALUE"):null;
+		 Double valeurexpertise = (data.get("VALEUR_EXPERTISE")!= "" && data.get("VALEUR_EXPERTISE")!=null)? Double.parseDouble(data.get("VALEUR_EXPERTISE")):null;
+		 String currencyValeur_expertise = (data.get("CURRENCY_VALEUR_EXPERTISE")!= "" && data.get("CURRENCY_VALEUR_EXPERTISE")!=null)? data.get("CURRENCY_VALEUR_EXPERTISE"):null;
+		 Double cashColValue = (data.get("CASH_COL_VALUE")!= "" && data.get("CASH_COL_VALUE")!=null)? Double.parseDouble(data.get("CASH_COL_VALUE")):null;
+		 String currencyCashColValue = (data.get("CURRENCY_CASH_COL_VALUE")!= "" && data.get("CURRENCY_CASH_COL_VALUE")!=null)? data.get("CURRENCY_CASH_COL_VALUE"):null;
+		 Double depbdzd = (data.get("depbdzd")!= "" && data.get("depbdzd")!=null)? Double.parseDouble(data.get("depbdzd")):null;
+		 Double depbdevise = (data.get("depbdevise")!= "" && data.get("depbdevise")!=null)? Double.parseDouble(data.get("depbdevise")):null;
+		 String currencyDepbDevise = (data.get("currencyDepbDevise")!= "" && data.get("currencyDepbDevise")!=null)? data.get("currencyDepbDevise"):null;
+		 Double depconf = (data.get("depconf")!= "" && data.get("depconf")!=null)? Double.parseDouble(data.get("depconf")):null;
+		 Double shareColValue = (data.get("SHARE_COL_VALUE")!= "" && data.get("SHARE_COL_VALUE")!=null)? Double.parseDouble(data.get("SHARE_COL_VALUE")):null;
+		 Double veicleColValue = (data.get("VEICLE_COL_VALUE")!= "" && data.get("VEICLE_COL_VALUE")!=null)? Double.parseDouble(data.get("VEICLE_COL_VALUE")):null;
+		 String currencyVehicleColValue = (data.get("CURRENCY_VEICLE_COL_VALUE")!= "" && data.get("CURRENCY_VEICLE_COL_VALUE")!=null)? data.get("CURRENCY_VEICLE_COL_VALUE"):null;
+		 Double  valeurVehicule = (data.get("VALEUR_VEHICULE")!= "" && data.get("VALEUR_VEHICULE")!=null)? Double.parseDouble(data.get("VALEUR_VEHICULE")):null;
+		 String currencyValeurVehicule = (data.get("CURRENCY_VALEUR_VEHICULE")!= "" && data.get("CURRENCY_VALEUR_VEHICULE")!=null)? data.get("CURRENCY_VALEUR_VEHICULE"):null;
+		 Double bankingLgColValue = (data.get("BANKING_LG_COL_VALUE")!= "" && data.get("BANKING_LG_COL_VALUE")!=null)? Double.parseDouble(data.get("BANKING_LG_COL_VALUE")):null;
+		 String currencyBankingLgColValue = (data.get("CURRENCY_BANKING_LG_COL_VALUE")!= "" && data.get("CURRENCY_BANKING_LG_COL_VALUE")!=null)? data.get("CURRENCY_BANKING_LG_COL_VALUE"):null;
+		 
+		 Garantie garantie = new Garantie(0,realEstateColValue, currencyRealEstateColValue, valeurexpertise, currencyValeur_expertise,
+				 cashColValue, currencyCashColValue, depbdzd, depbdevise, currencyDepbDevise, depconf, shareColValue, veicleColValue,
+				 currencyVehicleColValue, valeurVehicule, currencyValeurVehicule, bankingLgColValue,  currencyBankingLgColValue );
+		 
+		 return garantie;
+	}
 
 }
---
+++ gestion-risque-main/complete/application/src/main/java/it/gestionRisque/app/di/Controller/SeedTablesController.java
@@ -0,0 +1,26 @@
+package it.gestionRisque.app.di.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.gestionRisque.app.di.Services.SeedTablesService;
+
+
+
+@RestController
+@CrossOrigin("*")
+public class SeedTablesController {
+	
+private final SeedTablesService seedTables;
+	
+	public SeedTablesController(SeedTablesService seedTables) {
+		this.seedTables = seedTables;
+	}
+	
+	@PostMapping("/seed")
+	public void upload() throws Exception{
+		 seedTables.addToTables();
+	}
+
+}
