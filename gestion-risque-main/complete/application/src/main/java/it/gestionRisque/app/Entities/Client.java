f0d0ec19f07624369b1a234f5eff2e4aa34ba27a
abdelaziz.araar <h.araar@intervalle-technologies.com>
2022-12-19 09:09:04 +0100

    ajout mapping pour colonnes groupid, groupname, solId, solName

--- gestion-risque-main/complete/application/src/main/java/it/gestionRisque/app/Entities/Client.java
+++ gestion-risque-main/complete/application/src/main/java/it/gestionRisque/app/Entities/Client.java
@@ -49,16 +49,16 @@ private Integer entitieCode;
 @Column(name = "borrowerType")
 private String  borrowerType;
 
-@Column()
+@Column(name = "ID_GROUPE")
 private String groupId;
 
-@Column()
+@Column(name = "NOM_GROUPE")
 private String groupName;
 
-@Column()
+@Column(name = "ID_AGENCE")
 private String solId;
 
-@Column()
+@Column(name = "NOM_AGENCE")
 private String solDescription;
 
 @Column(name = "CLASS_PRINCIPALE_CLIENT")
